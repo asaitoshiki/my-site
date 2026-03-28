dad0502096d176076829dd332780e99a58b9789c
asaitoshiki <toshitarou0609@gmail.com>
2026-03-28 12:18:54 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '4rem' }}>
+      <h1>私のサイト</h1>
+      <p>ここに自己紹介や説明が入ります。</p>
+      <a href="/apps/zinyoku">禁欲アプリを使う →</a>
+    </div>
+  )
+}
+
+export default App
